4018eda4a6196902ab226b6014422c6d96d6ae07
borilio <exprimetuandroid@gmail.com>
2022-05-10 22:51:49 +0200

    Añadida clase de Testing para la clase DispensadorJabonTest.java

---
+++ src/test/java/com/inserta/utilidades/DispensadorJabonTest.java
@@ -0,0 +1,25 @@
+package com.inserta.utilidades;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DispensadorJabonTest {
+
+    /**
+     * TODO Probamos que al crear un dispensador por defecto, se cumpla lo siguiente...
+     * La prueba unitaria dará correcto si se cumplen las 3 condiciones. No pasará la prueba si
+     * se incumple alguna de las condiciones.
+     * <ul>
+     *     <li>La marca no es null</li>
+     *     <li>Está apagado</li>
+     *     <li>El depósito está vacío</li>
+     * </ul>
+     */
+    @Test
+    public void creacionPorDefecto(){
+        fail("No implementado");
+    }
+
+
+}
