b2cb7c37d04b1a5147ebd4f6786bde365dbd765a
borilio <exprimetuandroid@gmail.com>
2022-05-11 12:12:24 +0200

    Añadidas todas las pruebas unitarias para probar que se apaga correctamente, tanto manual como automáticamente

--- src/test/java/com/inserta/utilidades/DispensadorJabonTest.java
+++ src/test/java/com/inserta/utilidades/DispensadorJabonTest.java
@@ -28,10 +28,48 @@ public class DispensadorJabonTest {
      * Hacer una prueba unitaria para comprobar el dispensador se apaga automáticamente al llegar al 0% de batería
      */
     @Test
-    public void apagadoAutomatico(){
-
-        fail("No implementado todavía");
+    public void apagadoAutomaticoModificandoBateria(){
+        DispensadorJabon dispensadorJabon = new DispensadorJabon();
+        dispensadorJabon.encender();
+        System.out.println(dispensadorJabon);
+        System.out.println("Le ponemos la batería al 0");
+        dispensadorJabon.setBateria(0);
+        System.out.println(dispensadorJabon);
+        assertFalse("No se apagó automáticamente", dispensadorJabon.isEncendido());
     }
 
+    /**
+     * Hacer una prueba unitaria para comprobar el dispensador se apaga automáticamente al encenderlo muchas veces
+     */
+    @Test
+    public void apagadoAutomaticoEncendiendoloMucho(){
+        DispensadorJabon jabonera = new DispensadorJabon();
+        for (int i = 0; i <= 30000; i++) {
+            jabonera.encender();
+        }
+        assertTrue("Debería estar apagado", jabonera.isApagado());
+    }
+
+    /**
+     * Hacer una prueba unitaria para comprobar el dispensador se apaga automáticamente si se queda sin batería
+     * al dispensar muchas veces.
+     */
+    @Test
+    public void apagadoAutomaticoDispensarMucho(){
+        DispensadorJabon dispensadorJabon = new DispensadorJabon();
+        dispensadorJabon.encender();
+        for (int i = 0; i <= 30000; i++) {
+            dispensadorJabon.dispensar();
+        }
+        assertTrue("Debería estar apagado", dispensadorJabon.isApagado());
+    }
+
+    @Test
+    public void apagadoManualmenteConElBoton(){
+        DispensadorJabon dispensadorJabon = new DispensadorJabon();
+        dispensadorJabon.encender();
+        dispensadorJabon.apagar();
+        assertTrue("Debería estar apagado", dispensadorJabon.isApagado());
+    }
 
 }
